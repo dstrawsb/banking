b94f7e93a38c0bac5fcb0c7ff4a602536d662043
dstrawsb <don@strawsburg.com>
2013-03-24 16:19:24 -0400

    small

---
+++ Banking/src/com/ruben/Customer.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package com.ruben;
+
+/**
+ * @author Petr Ruben
+ *
+ *
+ * -------------------------
+ * Account in the Bank
+ * -------------------------
+ */
+public class Customer {
+
+	private String name = null;
+
+	private String password = null;
+	private boolean valid = false;
+	
+	public Customer(String aName, String aPassword) {
+		this.name = aName;
+		this.password = aPassword;
+		if(this.name != null && this.password != null) {
+			this.valid = true;
+		}
+	}
+	
+	/**
+	 * @return the name
+	 *
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the valid
+	 */
+	public boolean isValid() {
+		return valid;
+	}
+
+	/**
+	 * @param valid the valid to set
+	 */
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+}
---
+++ Banking/src/com/ruben/Login.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ruben;
+
+/**
+ * @author Petr Ruben
+ *
+ *
+ * -------------------------
+ * Login 
+ * -------------------------
+ */
+public class Login {
+
+	public Login(String username, String password) {
+		
+	}
+	
+}
