ff1ccf1a245d6c188b87b657af66676e13caeb66
dstrawsb <don@strawsburg.com>
2013-03-27 14:32:50 -0400

    Found the interface file

---
+++ Banking/src/com/ruben/NewInterface.java
@@ -0,0 +1,20 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author don
+ */
+public interface NewInterface extends ActionListener {
+
+    void actionPerformed(ActionEvent e);
+
+    void init();
+
+}
