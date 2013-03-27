9d22dc3e64e6ebf364b592dc2f34c0f0a0fdbe8a
dstrawsb <don@strawsburg.com>
2013-03-26 23:06:07 -0400

    put the executable code in a method

--- Banking/src/com/ruben/Testing.java
+++ Banking/src/com/ruben/Testing.java
@@ -1,6 +1,8 @@
 package com.ruben;
 
 public class Testing {
-	system.out.println (" I am Java and i will work properly");
+	public void needAMethod() {
+		System.out.println("I am Java and i will work properly");
+	}
 
 }
