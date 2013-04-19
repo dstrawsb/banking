3f1051761b030eea8961f5aab927c8038fc7ebb3
LostWindScar <petr.ruben@gmail.com>
2013-04-18 21:45:49 -0400

    c

---
+++ Banking/src/Mime.java
@@ -0,0 +1,15 @@
+
+public class Mime {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float z;
+z = 5 / 10; 
+System.out.print (z);
+
+	}
+
+}
